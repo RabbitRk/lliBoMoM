23cb6a3ca7a7946b915f10a1bf8bbbf313484271
RkRabbit <emailtork09@gmail.com>
2020-06-03 16:38:28 +0530

    Ui initiated

--- app/src/main/java/com/rabbitt/momobill/MainActivity.java
+++ app/src/main/java/com/rabbitt/momobill/MainActivity.java
@@ -1,14 +1,41 @@
 package com.rabbitt.momobill;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.rabbitt.momobill.demo.DemoFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "maluHomeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(getString(R.string.home));
+        loadFragment(new DemoFragment());
+    }
+
+    public Boolean loadFragment(final Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            Log.i(TAG, "Current thread: " + Thread.currentThread().getId());
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragmentcontainer, fragment)
+                    .addToBackStack(null)
+                    .commit();
+
+            return true;
+        }
+
+        return false;
     }
 }
